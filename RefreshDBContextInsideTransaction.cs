3c2c0dfe89886eb23448d3786e26d29b3876e428
Aridv <kuinatheauri@gmail.com>
2019-08-31 11:49:12 +0100

    First commit

---
+++ RefreshDBContextInsideTransaction.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace RefreshDBContextInsideTransaction
+{
+    class RefreshDBContextInsideTransaction
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var ctx = new context())
+                {
+                    ctx.Connection.Open();
+                    ctx.Transaction = ctx.Connection.BeginTransaction();
+
+                    ctx.Entities.InsertOnSubmit(itemDB);
+                    ctx.SubmitChanges();
+                    ctx.Refresh(RefreshMode.OverwriteCurrentValues, itemDB);
+
+                    Model newModel = itemDB.ToModel();
+
+                    ctx.Transaction.Commit();
+                    return newModel;
+                }
+            }
+            catch (Exception ex)
+            {
+                ctx.Transaction.Rollback();
+            }
+        }
+    }
+}
